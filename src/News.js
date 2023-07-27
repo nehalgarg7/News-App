d459c28af18fb2182c0395b69da070a4a963c14f
Nehal Garg <nehalgarg37@gmail.com>
2023-07-27 15:42:25 +0530

    News-App

--- src/News.js
+++ src/News.js
@@ -3,42 +3,60 @@ import NewsItem from './NewsItem'
 
 export default class News extends Component {
 
-    articles = [
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-            "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-            "publishedAt": "2020-04-27T11:41:47Z",
-            "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-            "source": {
-                "id": "espn-cric-info",
-                "name": "ESPN Cric Info"
-            },
-            "author": null,
-            "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-            "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-            "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-            "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-            "publishedAt": "2020-03-30T15:26:05Z",
-            "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-    ]
     constructor() {
         super();
         console.log("Hello i am constructor from News Component");
         this.state = {
-            articles: this.articles,
-            loading: false
+            articles: [],
+            loading: false,
+            page: 1
         }
     }
+
+
+    async componentDidMount() {
+        console.log("cdm");
+        let url = "https://newsapi.org/v2/everything?q=bitcoin&apiKey=b97491f757414ae790f094839f54b747&page=1&pagesize=20";
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
+
+    }
+
+    handlePrevClick = async () => {
+        console.log("Previous");
+
+        let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=b97491f757414ae790f094839f54b747&page=${(this.state.page + 1)}&pagesize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+
+
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
+    handleNextClick = async () => {
+        console.log("Next");
+
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+            let url = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=b97491f757414ae790f094839f54b747&page=${(this.state.page + 1)}&pagesize=20`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+
+
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+
+
 
     render() {
         return (
@@ -47,9 +65,14 @@ export default class News extends Component {
                     <h2>Top HeadLines</h2>
 
                     <div className="row">
-                        {this.state.articles.map((element) => { return (<div className="col-md-4" key={element.url}><NewsItem title={element.title.slice(0, 45)} description={element.description.slice(0, 88)} url={element.urlToImage} newsUrl={element.url} /></div>); })}
+                        {this.state.articles.map((element) => { return (<div className="col-md-4" key={element.url}><NewsItem title={element ? element.title.slice(0, 45) : ""} description={element ? element.description.slice(0, 88) : ""} url={element.urlToImage} newsUrl={element.url} /></div>); })}
 
                     </div>
+                </div>
+
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" class="btn btn-outline-dark btn-dark text-light" onClick={this.handlePrevClick}> &larr; Prev</button>
+                    <button disabled= {this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} type="button" class="btn btn-outline-dark btn-dark text-light" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
